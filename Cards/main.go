55f3bb43a76618ee9341214e4e99cdb05504d29a
AkshatShetty101 <akshatshetty2908@gmail.com>
2018-01-18 21:07:26 +0530

    Finished project functionality

--- Cards/main.go
+++ Cards/main.go
@@ -5,14 +5,29 @@ import (
 )
 
 func main() {
+	// Creating a new Deck
 	cards := newDeck()
+	fmt.Println("Newly created deck:-\n")
 	cards.print()
-	hand, remainingDeck := deal(cards, 5)
 	fmt.Println("-------------------------------------")
+	// Creating Hands
+	fmt.Println("Creating hands from the deck:-\n")
+	hand, remainingDeck := deal(cards, 5)
 	hand.print()
 	fmt.Println("-------------------------------------")
 	remainingDeck.print()
-	x := cards.toString()
-	fmt.Println(x)
-	cards.saveToFile("myCards.txt")
+	fmt.Println("-------------------------------------")
+	// Saving deck to File
+	fmt.Println("Saving current deck to file:-\n")
+	cards.saveToFile("myCards")
+	// Reading deck from file
+	fmt.Println("-------------------------------------")
+	fmt.Println("Loading deck from file:-\n")
+	d := newDeckFromFile("myCards")
+	fmt.Println(d)
+	fmt.Println("-------------------------------------")
+	// Shuffling the cards
+	fmt.Println("Shuffling the deck:-\n")
+	cards.shuffle()
+	cards.print()
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Just a set of basic projects implemented in Go
 
 Project Cards:-
-Aim- To implement a Go progrram to create a deck of cards, split it into two decks, shuffle it and store in a file.
+Aim- To implement a Go program to create a deck of cards, split it into two decks, shuffle it, store in a file and read from a file
